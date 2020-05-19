5c6b0745355d687544596ffd595c41bfe231642e
thefreshprince98 <Flemingjason01@gmail.com>
2020-05-19 19:02:29 -0400

    new html

---
+++ Public/app.js
@@ -0,0 +1,8 @@
+
+document.querySelector('.close').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "none";
+});
+
+$(".box").on("click",function(){
+document.querySelector('.bg-modal').style.display = "flex";	
+})
